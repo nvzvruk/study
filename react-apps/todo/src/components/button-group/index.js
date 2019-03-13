f00c6012498ae4eb93da59a4092f5aeca7cc81a5
nvzvruk <nazaruk44@gmail.com>
2019-03-13 08:42:20 +0200

    Refactor some components from Function to Class.

--- react-apps/todo/src/components/button-group/index.js
+++ react-apps/todo/src/components/button-group/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ButtonGroup = ({ buttonsData }) => {
+export default class ButtonGroup extends Component {
+    render() {
+        const { buttonsData } = this.props;
+        const buttons = buttonsData.map(({ label }, idx) => {
+            return(
+                <button key={`${label}-${idx}`}
+                        type="button"
+                        className="btn btn-outline-primary button-group__item">
+                    { label }
+                </button>
+            );
+        });
 
-    const buttons = buttonsData.map(({ label }, idx) => {
-        return (
-            <button key={`${label}-${idx}`}
-                    type="button"
-                    className="btn btn-outline-primary button-group__item">
-                { label }
-            </button>
+        return(
+            <div className="btn-group button-group" role="group" aria-label="Basic example">
+                { buttons }
+            </div>
         );
-    });
-
-    return (
-        <div className="btn-group button-group" role="group" aria-label="Basic example">
-            { buttons }
-        </div>
-    );
-};
-
-export default ButtonGroup
+    }
+}
